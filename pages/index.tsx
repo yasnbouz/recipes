07e32d7123d6fb27654704307ad43a847dc767c3
Yassine El Bouziady <45861586+yassineelbouziady@users.noreply.github.com>
2020-06-25 20:51:41 +0000

    render home page as ssg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 import MainLayout from 'components/layout/MainLayout';
 import { RecipeList, queryEnum } from 'components/RecipeList';
-
+import { initializeApollo } from 'lib/apolloClient';
+import { GetStaticProps } from 'next';
+import { RecipesGraphQlDocument } from 'generated/apollo-components';
 const Home = () => {
     return (
         <MainLayout title="Recipes">
             <RecipeList parentRoute="recipe" queryType={queryEnum.recipes} />
         </MainLayout>
     );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+    const apolloClient = initializeApollo();
+    await apolloClient.query({ query: RecipesGraphQlDocument });
+    return {
+        props: {
+            initialApolloState: apolloClient.cache.extract(),
+        },
+        unstable_revalidate: 1,
+    };
 };
 
 export default Home;
