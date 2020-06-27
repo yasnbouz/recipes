b392719a8bf69d610a8d71b0ed50571a9a1a92c3
Yassine El Bouziady <45861586+yassineelbouziady@users.noreply.github.com>
2020-06-27 18:58:14 +0000

    Add Auth0 Api

---
+++ pages/api/callback.ts
@@ -0,0 +1,11 @@
+import { NextApiResponse, NextApiRequest } from 'next';
+import auth0 from 'lib/auth0';
+
+export default async function callback(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        await auth0.handleCallback(req, res, { redirectTo: '/' });
+    } catch (error) {
+        console.error(error);
+        res.status(error.status || 500).end(error.message);
+    }
+}
---
+++ pages/api/logout.ts
@@ -0,0 +1,11 @@
+import auth0 from 'lib/auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function logout(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        await auth0.handleLogout(req, res);
+    } catch (error) {
+        console.error(error);
+        res.status(error.status || 500).end(error.message);
+    }
+}
