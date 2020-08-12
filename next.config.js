7dd651b8ae1ef0cd8f497f7bfcd259d509e3f5fb
yasnbouzi <elbouziady.yassine@gmail.com>
2020-08-12 11:41:51 +0100

    Adding Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
-module.exports = withPWA({
-    pwa: {
-        dest: 'public',
-        runtimeCaching,
-    },
-});
+
+module.exports = withBundleAnalyzer(
+    withPWA({
+        pwa: {
+            dest: 'public',
+            runtimeCaching,
+        },
+    }),
+);
